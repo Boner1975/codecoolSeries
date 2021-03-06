915b5f537779e7635d7ae631bafed8640c59cc4d
FabolousBishop <bartosz.tomaszewski.1994@gmail.com>
2021-03-06 16:29:05 +0100

    show page added

--- data/queries.py
+++ data/queries.py
@@ -23,3 +23,44 @@ def get_shows_by_parm(column='rating', order='desc', limit=0):
         """).format(column=sql.Identifier(column)),
         {'order': order, 'limit': limit}
     )
+
+
+def get_show_by_id(id):
+
+    return data_manager.execute_select(
+        sql.SQL("""
+            SELECT shows.id, shows.title, shows.year, shows.runtime, shows.homepage, shows.trailer,
+            to_char(shows.rating::float, '0.9') as rating,
+            string_agg(genres.name, ', ' ORDER BY genres.name) as genres
+            FROM shows
+            JOIN show_genres on shows.id = show_genres.show_id
+            JOIN genres on show_genres.genre_id = genres.id
+            WHERE shows.id = %(id)s
+            GROUP BY shows.id 
+        """),
+        {'id': id}, False)
+
+
+def get_show_seasons(id):
+
+    return data_manager.execute_select(
+        sql.SQL("""
+            SELECT s.season_number, s.title, s.overview
+            FROM shows
+            JOIN seasons s on shows.id = s.show_id
+            WHERE shows.id = %(id)s
+        """),
+        {'id': id})
+
+
+def get_show_characters(id, limit = 3):
+
+    return data_manager.execute_select(
+        sql.SQL("""
+            SELECT sc.id, sc.character_name, a.name, a.birthday, a.death, a.biography
+            FROM actors a
+            JOIN show_characters sc on a.id = sc.actor_id
+            WHERE sc.show_id = %(id)s
+            LIMIT %(limit)s
+        """),
+        {'id': id, 'limit': limit})
--- main.py
+++ main.py
@@ -23,9 +23,18 @@ def main():
 @app.route('/shows/most-rated')
 def most_rated(column='rating', order='desc', limit=15):
     shows = queries.get_shows_by_parm(column, order, limit)
-    print(shows)
     return render_template('shows.html', shows=shows)
 
+
+@app.route('/tv-show/<int:id>')
+def show_page(id):
+    show = queries.get_show_by_id(id)
+    seasons = queries.get_show_seasons(id)
+    characters = queries.get_show_characters(id)
+    show['characters_str'] = ', '.join([character['name'] for character in characters])
+    show['trailer_id'] = show['trailer'].split('=')[1] if show['trailer'] else ''
+    show['runtime_str'] = ''
+    return render_template('show.html', show = show, seasons = seasons)
 
 
 if __name__ == '__main__':
